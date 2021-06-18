2e97efdaf87c10f28c3a4d7a9aaef18cc8f6de11
Piotr Marecki <pm@aimachina.io>
2021-06-18 21:27:44 +0200

    malloc test

---
+++ algorytmy/malloc_tests/a.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+   cout << "hi" << endl;
+   int x = 0;
+   for(int i=0; i<1000; i++) {
+     vector<int> w(10000000);
+     x += w.size();
+   }
+   cout << x << endl;
+
+}
---
+++ algorytmy/malloc_tests/gg.py
@@ -0,0 +1,9 @@
+from algorytmy.utils import ts
+
+st = ts()
+x = 0
+for t in range(10 ** 3):
+    w = [0] * (10 ** 6)
+    x += len(w)
+
+print(x, f'{ts()-st:.3f}s')
